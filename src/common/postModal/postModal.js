b995cb3f41342e8e920a0df01a16996cc054d35f
Vicky Gupta <vicky.gupta@in.imshealth.com>
2021-04-26 00:10:07 +0530

    Adding like and toggle function

--- src/common/postModal/postModal.js
+++ src/common/postModal/postModal.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Modal, Button, TextField } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import avatar from "../../assets/download.png";
+import { Favorite, FavoriteBorder } from "@material-ui/icons";
 import "./postModal.css";
 
 const useStyles = theme => ({
@@ -19,32 +20,62 @@ class PostModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      postDetails: {}
+      postDetails: props.selectedPost,
+      newComment: ""
     };
   }
 
-  componentWillReceiveProps(nextProps) {
-    console.log("next props", nextProps.selectedPost);
+  updateComment = () => {
+    const { postDetails, newComment } = this.state;
+    const { onUpdatePost } = this.props;
+    this.setState(
+      {
+        postDetails: {
+          ...postDetails,
+          comments: [...postDetails.comments, newComment]
+        },
+        newComment: ""
+      },
+      () => {
+        onUpdatePost(this.state.postDetails);
+      }
+    );
+  };
+
+  toggleLike = isLiked => {
+    const { postDetails, newComment } = this.state;
+    const { onUpdatePost } = this.props;
+    this.setState(
+      {
+        postDetails: {
+          ...postDetails,
+          isLiked: isLiked,
+          likeCount: isLiked
+            ? postDetails.likeCount + 1
+            : postDetails.likeCount - 1
+        }
+      },
+      () => {
+        onUpdatePost(this.state.postDetails);
+      }
+    );
+  };
+
+  onChangeComment = e => {
     this.setState({
-      postDetails: nextProps.selectedPost
+      newComment: e.target.value
     });
-  }
+  };
 
   render() {
-    const {
-      visible,
-      onClose,
-      onUpdateLikes,
-      onUpdateComment,
-      classes
-    } = this.props;
+    const { visible, onClose, classes } = this.props;
 
-    const {postDetails}=this.state;
+    const { postDetails, newComment } = this.state;
 
     console.log(postDetails);
     return (
       <Modal open={visible} onClose={onClose} className="post-modal">
-       <div
+        <div
           style={{
             top: "50%",
             left: "50%",
@@ -54,10 +85,67 @@ class PostModal extends Component {
           className={classes.paper}
         >
           <div className="post-img">
-            <img src={postDetails&&postDetails.media_url} alt="img" />
+            <img src={postDetails && postDetails.media_url} alt="img" />
           </div>
           <div className="post-info">
+            <div className="post-top-info">
+              <div className="post-header">
+                <div className="post-avatar">
+                  <img src={avatar} alt="avatar" />
+                </div>
+                <div className="post-username">{postDetails.username}</div>
+              </div>
+              <hr />
 
+              <div className="post-caption">{postDetails.caption}</div>
+              <div className="post-hastag">{postDetails.hashtag}</div>
+              <div className="post-comments">
+                {postDetails.comments.map(comment => {
+                  return (
+                    <div className="comments">
+                      <span className="username">{postDetails.username}:</span>{" "}
+                      <span className="comment">{comment}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="post-footer">
+              <div className="like-button">
+                <div
+                  className="like-post"
+                  onClick={() => this.toggleLike(!postDetails.isLiked)}
+                >
+                  {postDetails.isLiked ? (
+                    <Favorite style={{ color: "red" }} />
+                  ) : (
+                    <FavoriteBorder />
+                  )}
+                </div>
+                {postDetails.likeCount ? (
+                  <div>
+                    {postDetails.likeCount === 1
+                      ? "1 like"
+                      : `${postDetails.likeCount} likes`}
+                  </div>
+                ) : null}
+              </div>
+              <div className="comment-container">
+                <TextField
+                  label="Add a comment"
+                  value={newComment}
+                  onChange={e => this.onChangeComment(e)}
+                  style={{ marginRight: "5px", width: "80%" }}
+                />
+                <Button
+                  color="primary"
+                  onClick={() => this.updateComment()}
+                  variant="contained"
+                >
+                  Add
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </Modal>
--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -18,46 +18,46 @@ class Profile extends Component {
       openSelectedPostModal: false,
       selectedPost: {},
       posts: [
-        {
-          id: "17914978075280650",
-          media_type: "CAROUSEL_ALBUM",
-          media_url:
-            "https://scontent.cdninstagram.com/v/t51.2885-15/50890091_145134333162385_1575083287405871725_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=0IcQV8XTaT0AX_MBFEq&_nc_ht=scontent.cdninstagram.com&oh=4a24679e0424bc47f5053e0e6f19b89f&oe=60AA2F96",
-          username: "somya.summi",
-          timestamp: "2019-02-12T16:07:23+0000"
-        },
-        {
-          id: "17997724777263783",
-          media_type: "CAROUSEL_ALBUM",
-          media_url:
-            "https://scontent.cdninstagram.com/v/t51.2885-15/70352771_141261790465719_2947905106429861981_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=PlvyBK5kKU8AX-aQTqZ&_nc_ht=scontent.cdninstagram.com&oh=1b9710212f23a83e76141fd5c6d6ae97&oe=60A9877D",
-          username: "somya.summi",
-          timestamp: "2019-10-07T15:58:33+0000"
-        },
-        {
-          id: "17856234643778689",
-          media_type: "CAROUSEL_ALBUM",
-          media_url:
-            "https://scontent.cdninstagram.com/v/t51.2885-15/84569573_191405035598382_1329757645482833876_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=fSAZuV3PXGQAX_Nd4C_&_nc_ht=scontent.cdninstagram.com&oh=8ee30d5f7deda6bf93f1c6898a898313&oe=60AA1FC1",
-          username: "somya.summi",
-          timestamp: "2020-02-11T07:02:05+0000"
-        },
-        {
-          id: "17921112409429366",
-          media_type: "CAROUSEL_ALBUM",
-          media_url:
-            "https://scontent.cdninstagram.com/v/t51.29350-15/111119622_1179117219137373_3261375636624685940_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=UISZjKM4GxIAX9LoCY4&_nc_ht=scontent.cdninstagram.com&oh=e34f49eec1ddfffd5e634420d03e35da&oe=60A7D47C",
-          username: "somya.summi",
-          timestamp: "2020-07-18T20:13:07+0000"
-        },
-        {
-          id: "17904000214522433",
-          media_type: "CAROUSEL_ALBUM",
-          media_url:
-            "https://scontent.cdninstagram.com/v/t51.29350-15/118702473_4237732872968665_5244809759490456506_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=7Qh8gQX_yecAX8rsAGq&_nc_oc=AQnFvtkSCiDbpDihXCyn3DlHX78r5ETUtWbGXxiMo0CPTE7b8MDgpSbA2xkfqvVt1DwyYMP_wLdLhH8PphtU3ghX&_nc_ht=scontent.cdninstagram.com&oh=22efc80fec5f46db0dd723d8f4ed05fc&oe=60A838CB",
-          username: "somya.summi",
-          timestamp: "2020-09-01T06:44:59+0000"
-        }
+        // {
+        //   id: "17914978075280650",
+        //   media_type: "CAROUSEL_ALBUM",
+        //   media_url:
+        //     "https://scontent.cdninstagram.com/v/t51.2885-15/50890091_145134333162385_1575083287405871725_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=0IcQV8XTaT0AX_MBFEq&_nc_ht=scontent.cdninstagram.com&oh=4a24679e0424bc47f5053e0e6f19b89f&oe=60AA2F96",
+        //   username: "somya.summi",
+        //   timestamp: "2019-02-12T16:07:23+0000"
+        // },
+        // {
+        //   id: "17997724777263783",
+        //   media_type: "CAROUSEL_ALBUM",
+        //   media_url:
+        //     "https://scontent.cdninstagram.com/v/t51.2885-15/70352771_141261790465719_2947905106429861981_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=PlvyBK5kKU8AX-aQTqZ&_nc_ht=scontent.cdninstagram.com&oh=1b9710212f23a83e76141fd5c6d6ae97&oe=60A9877D",
+        //   username: "somya.summi",
+        //   timestamp: "2019-10-07T15:58:33+0000"
+        // },
+        // {
+        //   id: "17856234643778689",
+        //   media_type: "CAROUSEL_ALBUM",
+        //   media_url:
+        //     "https://scontent.cdninstagram.com/v/t51.2885-15/84569573_191405035598382_1329757645482833876_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=fSAZuV3PXGQAX_Nd4C_&_nc_ht=scontent.cdninstagram.com&oh=8ee30d5f7deda6bf93f1c6898a898313&oe=60AA1FC1",
+        //   username: "somya.summi",
+        //   timestamp: "2020-02-11T07:02:05+0000"
+        // },
+        // {
+        //   id: "17921112409429366",
+        //   media_type: "CAROUSEL_ALBUM",
+        //   media_url:
+        //     "https://scontent.cdninstagram.com/v/t51.29350-15/111119622_1179117219137373_3261375636624685940_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=UISZjKM4GxIAX9LoCY4&_nc_ht=scontent.cdninstagram.com&oh=e34f49eec1ddfffd5e634420d03e35da&oe=60A7D47C",
+        //   username: "somya.summi",
+        //   timestamp: "2020-07-18T20:13:07+0000"
+        // },
+        // {
+        //   id: "17904000214522433",
+        //   media_type: "CAROUSEL_ALBUM",
+        //   media_url:
+        //     "https://scontent.cdninstagram.com/v/t51.29350-15/118702473_4237732872968665_5244809759490456506_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=7Qh8gQX_yecAX8rsAGq&_nc_oc=AQnFvtkSCiDbpDihXCyn3DlHX78r5ETUtWbGXxiMo0CPTE7b8MDgpSbA2xkfqvVt1DwyYMP_wLdLhH8PphtU3ghX&_nc_ht=scontent.cdninstagram.com&oh=22efc80fec5f46db0dd723d8f4ed05fc&oe=60A838CB",
+        //   username: "somya.summi",
+        //   timestamp: "2020-09-01T06:44:59+0000"
+        // }
       ]
     };
   }
@@ -65,9 +65,10 @@ class Profile extends Component {
   componentDidMount() {
     if (!sessionStorage.userAuth) {
       this.props.history.push("/");
-    }
-    else {
-        
+    } else {
+      this.setState({
+        posts: JSON.parse(localStorage.dataSetMain)
+      });
     }
   }
 
@@ -82,10 +83,13 @@ class Profile extends Component {
       fullname: updatedName
     });
   };
-  openSelectedPost = selectedPost => {
+  openSelectedPost = (selectedPost, index) => {
     this.setState({
       openSelectedPostModal: true,
-      selectedPost
+      selectedPost: {
+        ...selectedPost,
+        index: index
+      }
     });
   };
   closeSelectedPost = () => {
@@ -94,6 +98,18 @@ class Profile extends Component {
       selectedPost: {}
     });
   };
+
+  updatePost=updatedPost=>{
+      const {posts}=this.state;
+      console.log("posts",posts);
+      const newPost=updatedPost;
+    //   delete newPost.index;
+      posts[updatedPost.index]=newPost;
+      console.log(newPost,posts);
+      this.setState({
+        posts
+      })
+  }
 
   render() {
     const {
@@ -111,13 +127,16 @@ class Profile extends Component {
           onClose={() => this.handleEditNameModal()}
           onUpdate={updatedName => this.saveFullName(updatedName)}
         />
-        <PostModal
-          selectedPost={selectedPost}
-          visible={openSelectedPostModal}
-          onClose={() => this.closeSelectedPost()}
-          onUpdateLikes={() => console.log()}
-          onUpdateComment={() => console.log()}
-        />
+        {openSelectedPostModal ? (
+          <PostModal
+            selectedPost={selectedPost}
+            visible={openSelectedPostModal}
+            onClose={() => this.closeSelectedPost()}
+            onUpdatePost={(updatedPost) => this.updatePost(updatedPost)}
+          />
+        ) : (
+          ""
+        )}
         <Header isProfile props={this.props} />
         <div className="profile-container">
           <div className="profile-header">
@@ -145,14 +164,16 @@ class Profile extends Component {
           </div>
           <div className="profile-body">
             <GridList cellHeight={180} className="grid-list">
-              {posts.map(post => (
-                <GridListTile
-                  key={post.id}
-                  onClick={() => this.openSelectedPost(post)}
-                >
-                  <img src={post.media_url} alt={post.id} />
-                </GridListTile>
-              ))}
+              {posts.map((post, index) => {
+                return (
+                  <GridListTile
+                    key={post.id}
+                    onClick={() => this.openSelectedPost(post, index)}
+                  >
+                    <img src={post.media_url} alt={post.id} />
+                  </GridListTile>
+                );
+              })}
             </GridList>
           </div>
         </div>
