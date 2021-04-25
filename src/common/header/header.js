9f8199a2a43d8bb4da34308295bbf79da8c90332
Vicky Gupta <vicky.gupta@in.imshealth.com>
2021-04-25 15:30:58 +0530

    Refactor header

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -1,16 +1,42 @@
+import React from "react";
 import { Grid, TextField, Typography, InputAdornment } from "@material-ui/core";
 import { Search, Face } from "@material-ui/icons";
-import React from "react";
+import avatar from '../../assets/download.png';
 import "./header.css";
 
 const Header = props => {
   return (
     <header>
-      <Grid container justify="space-between" alignItems="center">
+      <div className="header-container">
+        <div className="header-title">
+          Image Viewer
+        </div>
+
+        <div className="header-action">
+          <TextField
+            id="input-with-icon-textfield"
+            placeholder="Search..."
+            variant="filled"
+            value={props.value}
+            onChange={props.onChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              )
+            }}
+          />
+          <div className="avatar">
+            <img src={avatar} />
+          </div>
+        </div>
+      </div>
+      {/* <Grid container justify="space-between" alignItems="center">
         <Grid item>
           <Typography>Image Viewer</Typography>
         </Grid>
-        {props.isUser ? (
+        {props.isUser ? (<>
         <Grid item>
           <TextField
             id="input-with-icon-textfield"
@@ -26,10 +52,11 @@ const Header = props => {
               )
             }}
           />
-          <Face />
+          
         </Grid>
+        <Face /></>
         ) :null}
-      </Grid>
+      </Grid> */}
     </header>
   );
 };
